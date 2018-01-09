b707538ed4623804e0b762c1d1c6c7acc1b9f630
Jon Gardiner <jon@bamboohr.com>
2018-01-09 10:14:08 -0700

    Speed up defined constant check by using key/value lookups and by checking for "true", "false", "null" before doing any db checks.

--- src/Checks/DefinedConstantCheck.php
+++ src/Checks/DefinedConstantCheck.php
@@ -30,52 +30,63 @@ class DefinedConstantCheck extends BaseCheck {
 	 * @var array
 	 */
 	static private $phpConstants = [
-		"PHP_VERSION",
-		"PHP_MAJOR_VERSION",
-		"PHP_MINOR_VERSION",
-		"PHP_RELEASE_VERSION",
-		"PHP_VERSION_ID",
-		"PHP_EXTRA_VERSION",
-		"PHP_ZTS",
-		"PHP_DEBUG",
-		"PHP_MAXPATHLEN",
-		"PHP_OS",
-		"PHP_SAPI",
-		"PHP_EOL",
-		"PHP_INT_MAX",
-		"PHP_INT_MIN",
-		"PHP_INT_SIZE",
-		"DEFAULT_INCLUDE_SIZE",
-		"PEAR_INSTALL_DIR",
-		"PEAR_EXTENSION_DIR",
-		"PHP_EXTENSION_DIR",
-		"PHP_PREFIX",
-		"PHP_BINDIR",
-		"PHP_BINARY",
-		"PHP_MANDIR",
-		"PHP_LIBDIR",
-		"PHP_DATADIR",
-		"PHP_SYSCONFDIR",
-		"PHP_LOCALSTATEDIR",
-		"PHP_CONFIG_FILE_PATH",
-		"PHP_CONFIG_FILE_SCAN_DIR",
-		"PHP_SHLIB_SUFFIX",
-		"E_ERROR",
-		"E_WARNING",
-		"E_PARSE",
-		"E_NOTICE",
-		"E_CORE_ERROR",
-		"E_CORE_WARNING",
-		"E_COMPILE_ERROR",
-		"E_COMPILE_WARNING",
-		"E_USER_ERROR",
-		"E_USER_WARNING",
-		"E_USER_NOTICE",
-		"E_DEPRECATED",
-		"E_USER_DEPRECATED",
-		"E_ALL",
-		"E_STRICT",
-		"__COMPILER_HALT_OFFSET__"
+		"PHP_VERSION" => 1,
+		"PHP_MAJOR_VERSION" => 1,
+		"PHP_MINOR_VERSION" => 1,
+		"PHP_RELEASE_VERSION" => 1,
+		"PHP_VERSION_ID" => 1,
+		"PHP_EXTRA_VERSION" => 1,
+		"PHP_ZTS" => 1,
+		"PHP_DEBUG" => 1,
+		"PHP_MAXPATHLEN" => 1,
+		"PHP_OS" => 1,
+		"PHP_SAPI" => 1,
+		"PHP_EOL" => 1,
+		"PHP_INT_MAX" => 1,
+		"PHP_INT_MIN" => 1,
+		"PHP_INT_SIZE" => 1,
+		"DEFAULT_INCLUDE_SIZE" => 1,
+		"PEAR_INSTALL_DIR" => 1,
+		"PEAR_EXTENSION_DIR" => 1,
+		"PHP_EXTENSION_DIR" => 1,
+		"PHP_PREFIX" => 1,
+		"PHP_BINDIR" => 1,
+		"PHP_BINARY" => 1,
+		"PHP_MANDIR" => 1,
+		"PHP_LIBDIR" => 1,
+		"PHP_DATADIR" => 1,
+		"PHP_SYSCONFDIR" => 1,
+		"PHP_LOCALSTATEDIR" => 1,
+		"PHP_CONFIG_FILE_PATH" => 1,
+		"PHP_CONFIG_FILE_SCAN_DIR" => 1,
+		"PHP_SHLIB_SUFFIX" => 1,
+		"E_ERROR" => 1,
+		"E_WARNING" => 1,
+		"E_PARSE" => 1,
+		"E_NOTICE" => 1,
+		"E_CORE_ERROR" => 1,
+		"E_CORE_WARNING" => 1,
+		"E_COMPILE_ERROR" => 1,
+		"E_COMPILE_WARNING" => 1,
+		"E_USER_ERROR" => 1,
+		"E_USER_WARNING" => 1,
+		"E_USER_NOTICE" => 1,
+		"E_DEPRECATED" => 1,
+		"E_USER_DEPRECATED" => 1,
+		"E_ALL" => 1,
+		"E_STRICT" => 1,
+		"__COMPILER_HALT_OFFSET__" => 1
+	];
+
+	static $magicConstants = [
+		'__LINE__' => 1,
+		'__FILE__' => 1,
+		'__DIR__' => 1,
+		'__FUNCTION__' => 1,
+		'__CLASS__' => 1,
+		'__TRAIT__' => 1,
+		'__METHOD__' => 1,
+		'__NAMESPACE__' => 1
 	];
 
 	/**
@@ -115,13 +126,11 @@ class DefinedConstantCheck extends BaseCheck {
 	 * @return bool
 	 */
 	public function isLanguageConst($name) {
-		$consts = ["null","true","false"];
-		foreach ($consts as $const) {
-			if (strcasecmp($const, $name) == 0) {
-				return true;
-			}
-		}
-		return in_array($name, static::$phpConstants);
+		return
+			strcasecmp($name, 'null') == 0 ||
+			strcasecmp($name, 'true') == 0 ||
+			strcasecmp($name, 'false') == 0 ||
+			array_key_exists($name, DefinedConstantCheck::$phpConstants);
 	}
 
 	/**
@@ -132,7 +141,7 @@ class DefinedConstantCheck extends BaseCheck {
 	 * @return bool
 	 */
 	public function isMagicConstant($name) {
-		return in_array($name, ['__LINE__','__FILE__','__DIR__','__FUNCTION__', '__CLASS__','__TRAIT__','__METHOD__','__NAMESPACE__']);
+		return array_key_exists($name, DefinedConstantCheck::$magicConstants);
 	}
 
 	/**
@@ -158,8 +167,8 @@ class DefinedConstantCheck extends BaseCheck {
 	 */
 	public function run($fileName, Node $node, ClassLike $inside=null, Scope $scope=null) {
 		if ($node instanceof ConstFetch) {
-			$name = $node->name;
-			if (!$this->symbolTable->isDefined($name) && !$this->isLanguageConst($name) && !$this->isMagicConstant($name) && !$this->isExtensionConstant($name)) {
+			$name = strval($node->name);
+			if (!$this->isLanguageConst($name) && !$this->isMagicConstant($name) && !$this->isExtensionConstant($name) && !$this->symbolTable->isDefined($name)) {
 				$this->emitError($fileName, $node, ErrorConstants::TYPE_UNKNOWN_GLOBAL_CONSTANT, "That's not a thing.  Can't find define named \"$name\"");
 				return;
 			}
