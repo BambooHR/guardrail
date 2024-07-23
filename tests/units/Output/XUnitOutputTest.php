550cb95a28ba7768f7299fddbf2a4e3b60a0a9f7
Dalton Bennett <dbennett@bamboohr.com>
2024-07-23 08:22:16 -0600

    add another test

--- tests/units/Output/XUnitOutputTest.php
+++ tests/units/Output/XUnitOutputTest.php
@@ -11,12 +11,12 @@ use PHPUnit\Framework\TestCase;
 class XUnitOutputTest extends TestCase {
 
 	/**
-	 * @dataProvider shouldEmitProvider
+	 * @dataProvider shouldEmitIgnoreProvider
 	 */
 	public function testShouldEmitIgnoreArray($fileName, $expected) : void {
 		$emitList = [
 			[
-				'ignore' => ['**/app/BambooHR/Silo/Benefits/Shared/**/*'],
+				'ignore' => ['**/app/BambooHR/Controller/TimeOff/**/*', '**/app/BambooHR/Silo/Benefits/Shared/**/*'],
 			]
 		];
 		$config = new TestConfig('', $emitList);
@@ -25,10 +25,32 @@ class XUnitOutputTest extends TestCase {
 		self::assertEquals($expected, $actual);
 	}
 
-	public function shouldEmitProvider() {
+	public function shouldEmitIgnoreProvider() {
 		return [
 			['app/BambooHR/Silo/Benefits/Shared/BenefitCalculator/Settings/GroupPlanCalculatorSetting/RateSplittableGroupPlanCalculatorSetting.php', false],
 			['app/BambooHR/Controller/SomeTestOne.php', true]
+		];
+	}
+
+	/**
+	 * @dataProvider shouldEmitGlobProvider
+	 */
+	public function testShouldEmitGlobArray($fileName, $expected) : void {
+		$emitList = [
+			[
+				'glob' => ['**/app/BambooHR/Silo/Benefits/Shared/**/*'],
+			]
+		];
+		$config = new TestConfig('', $emitList);
+		$output = new XUnitOutput($config);
+		$actual = $output->shouldEmit($fileName, '', 0);
+		self::assertEquals($expected, $actual);
+	}
+
+	public function shouldEmitGlobProvider() {
+		return [
+			['app/BambooHR/Silo/Benefits/Shared/BenefitCalculator/Settings/GroupPlanCalculatorSetting/RateSplittableGroupPlanCalculatorSetting.php', true],
+			['app/BambooHR/Controller/SomeTestOne.php', false]
 		];
 	}
 
