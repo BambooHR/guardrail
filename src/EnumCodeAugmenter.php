8d5375f9303ca8838c7a756dc65e34f813ac5a92
Steven Hayman <shayman@bamboohr.com>
2025-09-27 13:32:58 -0500

    Fix formatting

--- src/EnumCodeAugmenter.php
+++ src/EnumCodeAugmenter.php
@@ -6,10 +6,8 @@ use PhpParser\Node;
 use PhpParser\Builder\Property;
 use PhpParser\Builder\Param;
 
-class EnumCodeAugmenter
-{
-	static public function addEnumPropsAndMethods(Node\Stmt\Enum_ $enum)
-	{
+class EnumCodeAugmenter {
+	static public function addEnumPropsAndMethods(Node\Stmt\Enum_ $enum) {
 		$isBacked = !is_null($enum->scalarType);
 		$property = new Property("name");
 		$property->setType(new Node\Identifier("string"));
@@ -17,16 +15,16 @@ class EnumCodeAugmenter
 		$enum->stmts[] = $property->getNode();
 		$enum->stmts[] = new Node\Stmt\ClassMethod("cases", ["returnType" => "array", "flags" => Node\Stmt\Class_::MODIFIER_PUBLIC | Node\Stmt\Class_::MODIFIER_STATIC]);
 		if ($isBacked) {
-			$enum->stmts[] = new Node\Stmt\ClassMethod("values", ["returnType" => "array"]);
+			$enum->stmts[] = new Node\Stmt\ClassMethod("values",["returnType"=>"array"]);
 			$property = new Property("value");
 			$property->makeReadonly();
-			$property->setType($enum->scalarType);
-			$enum->stmts[] = $property->getNode();
+			$property->setType( $enum->scalarType );
+			$enum->stmts[]=$property->getNode();
 
 			$enumName = $enum->namespacedName->toString();
 			$param = (new Param("fromValue"))->setType($enum->scalarType);
-			$enum->stmts[] = new Node\Stmt\ClassMethod("tryFrom", ["returnType" => $enumName, "flags" => Node\Stmt\Class_::MODIFIER_STATIC, 'params' => [$param->getNode()]]);
-			$enum->stmts[] = new Node\Stmt\ClassMethod("from", ["returnType" => $enumName, "flags" => Node\Stmt\Class_::MODIFIER_STATIC, 'params' => [$param->getNode()]]);
+			$enum->stmts[]=new Node\Stmt\ClassMethod("tryFrom",["returnType" => $enumName, "flags"=>Node\Stmt\Class_::MODIFIER_STATIC, 'params'=>[$param->getNode()]]);
+			$enum->stmts[]=new Node\Stmt\ClassMethod("from", ["returnType" => $enumName, "flags"=>Node\Stmt\Class_::MODIFIER_STATIC, 'params'=>[$param->getNode()]]);
 		}
 	}
 }
