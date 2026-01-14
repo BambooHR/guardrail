5bac2923d104578998c50720ce867ab89911fed7
fladolcetta <Fladolcetta@bamboohr.com>
2026-01-14 09:38:40 -0600

    TDD for the win

---
+++ tests/units/Checks/TestGenerator.php
@@ -0,0 +1,21 @@
+<?php namespace BambooHR\Guardrail\Tests\Checks;
+
+use BambooHR\Guardrail\Checks\ErrorConstants;
+use BambooHR\Guardrail\Tests\TestSuiteSetup;
+
+
+/**
+ * Class TestFunctionCalCheck
+ *
+ * @package BambooHR\Guardrail\Tests\Checks
+ */
+class TestGenerator extends TestSuiteSetup {
+
+	public function testGeneratorShouldPass() {
+		$this->assertEquals(0, $this->runAnalyzerOnFile('.1.inc', ErrorConstants::TYPE_SIGNATURE_RETURN), "Failed to validate generator return type" );
+	}
+
+	public function testGeneratorShouldFail() {
+		$this->assertEquals(1, $this->runAnalyzerOnFile('.2.inc', ErrorConstants::TYPE_SIGNATURE_RETURN), "Failed to validate generator return type" );
+	}
+}
