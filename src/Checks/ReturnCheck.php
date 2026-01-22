ee5b1914ad5f5f6359a63f29ca575ecc46830f4d
fladolcetta <Fladolcetta@bamboohr.com>
2026-01-22 17:19:05 -0600

    Add new empty return checks

--- src/Checks/ReturnCheck.php
+++ src/Checks/ReturnCheck.php
@@ -9,8 +9,11 @@ use BambooHR\Guardrail\Output\OutputInterface;
 use BambooHR\Guardrail\Scope;
 use BambooHR\Guardrail\SymbolTable\SymbolTable;
 use BambooHR\Guardrail\TypeComparer;
+use BambooHR\Guardrail\Util;
 use PhpParser\Node;
 use PhpParser\Node\Stmt\ClassLike;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\Stmt\Return_;
 
 /**
@@ -38,7 +41,7 @@ class ReturnCheck extends BaseCheck {
 	 * @return array
 	 */
 	public function getCheckNodeTypes() {
-		return [ Return_::class ];
+		return [ Return_::class, Function_::class, ClassMethod::class ];
 	}
 
 	/**
@@ -52,7 +55,9 @@ class ReturnCheck extends BaseCheck {
 	 * @return void
 	 */
 	public function run($fileName, Node $node, ?ClassLike $inside = null, ?Scope $scope = null) {
-		if ($node instanceof Return_) {
+		if ($node instanceof Function_ || $node instanceof ClassMethod) {
+			$this->checkFunctionForMissingReturn($fileName, $node, $inside);
+		} else if ($node instanceof Return_) {
 			$insideFunc = $scope?->getInsideFunction();
 
 			if (!$insideFunc) {
@@ -96,6 +101,74 @@ class ReturnCheck extends BaseCheck {
 		}
 	}
 
+
+	/**
+	 * Check if a function/method is missing a required return statement
+	 *
+	 * @param string                $fileName The file being checked
+	 * @param Function_|ClassMethod $node     The function or method node
+	 * @param ClassLike|null        $inside   The class we're inside of (if any)
+	 *
+	 * @return void
+	 */
+	protected function checkFunctionForMissingReturn($fileName, $node, ?ClassLike $inside = null) {
+		$returnType = $node->getReturnType();
+
+		if (!$returnType) {
+			return;
+		}
+
+		if ($node instanceof ClassMethod && $node->isAbstract()) {
+			return;
+		}
+
+		if ($node->stmts === null) {
+			return;
+		}
+
+		if ($this->returnTypeAllowsNoReturn($returnType)) {
+			return;
+		}
+
+		if (!Util::allBranchesExit($node->stmts)) {
+			$functionName = $this->getFunctionName($node, $inside);
+			$this->emitError($fileName, $node, ErrorConstants::TYPE_SIGNATURE_RETURN, "Function $functionName must return a value of type " . TypeComparer::typeToString($returnType));
+		}
+	}
+
+	/**
+	 * Check if a return type allows a function to have no return statement
+	 *
+	 * @param Node\Identifier|Node\Name|Node\ComplexType|null $returnType
+	 *
+	 * @return bool
+	 */
+	protected function returnTypeAllowsNoReturn($returnType): bool {
+		if (!$returnType) {
+			return true;
+		}
+
+		$allowedTypes = ["void", "never", "mixed", "none", "null"];
+		foreach ($allowedTypes as $type) {
+			if (TypeComparer::isNamedIdentifier($returnType, $type)) {
+				return true;
+			}
+		}
+
+		if ($returnType instanceof Node\NullableType) {
+			return true;
+		}
+
+		if ($returnType instanceof Node\UnionType) {
+			foreach ($returnType->types as $type) {
+				if (TypeComparer::isNamedIdentifier($type, "null")) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
 
 	/**
 	 * @param Node\FunctionLike $insideFunc The method we're inside of
