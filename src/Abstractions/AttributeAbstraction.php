2729147cabb7c0bfb6451811c0936a69cf643b0c
Frank Ladolcetta <SirJimboIII@gmail.com>
2026-01-30 16:49:32 -0600

    Fix PHPCS Failure

--- src/Abstractions/AttributeAbstraction.php
+++ src/Abstractions/AttributeAbstraction.php
@@ -5,17 +5,13 @@ namespace BambooHR\Guardrail\Abstractions;
 use PhpParser\Node\Attribute as NodeAttribute;
 
 readonly class AttributeAbstraction implements AttributeInterface {
-	public function __construct(private NodeAttribute $attr)
-	{
-	}
+	public function __construct(private NodeAttribute $attr) {}
 
-	public function getClassName(): string
-	{
+	public function getClassName(): string {
 		return $this->attr->name->toString();
 	}
 
-	public function getArguments(): array
-	{
+	public function getArguments(): array {
 		return $this->attr->args;
 	}
 }
