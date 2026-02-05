ec0ae0551d898a98b08b778b80fe8e03f3532abc
fladolcetta <Fladolcetta@bamboohr.com>
2026-02-05 17:39:29 -0600

    Split tests correctly

--- tests/units/Checks/TestConstFetch.php
+++ tests/units/Checks/TestConstFetch.php
@@ -17,7 +17,7 @@ class TestConstFetch extends TestSuiteSetup
 	 * @return void
 	 */
 	public function testRuntimeConstantTypes() {
-		$this->assertEquals(0, $this->runAnalyzerOnFile('.1.inc', ''));
+		$this->assertEquals(0, $this->runAnalyzerOnFile('.php-consts.inc', ''));
 	}
 
 	/**
@@ -26,6 +26,42 @@ class TestConstFetch extends TestSuiteSetup
 	 * @return void
 	 */
 	public function testBooleanAndNullConstants() {
-		$this->assertEquals(0, $this->runAnalyzerOnFile('.2.inc', ''));
+		$this->assertEquals(0, $this->runAnalyzerOnFile('.bool-null.inc', ''));
+	}
+
+	/**
+	 * Test that define() constants are correctly typed
+	 *
+	 * @return void
+	 */
+	public function testDefineConstants() {
+		$this->assertEquals(0, $this->runAnalyzerOnFile('.define-consts.inc', ''));
+	}
+
+	/**
+	 * Test basic global constant types
+	 *
+	 * @return void
+	 */
+	public function testBasicGlobalConstantTypes() {
+		$this->assertEquals(0, $this->runAnalyzerOnFile('.basic-types.inc', ''));
+	}
+
+	/**
+	 * Test bitwise operations in global constants
+	 *
+	 * @return void
+	 */
+	public function testBitwiseOperationsInGlobalConstants() {
+		$this->assertEquals(0, $this->runAnalyzerOnFile('.bitwise-ops.inc', ''));
+	}
+
+	/**
+	 * Test negative values in global constants
+	 *
+	 * @return void
+	 */
+	public function testNegativeValuesInGlobalConstants() {
+		$this->assertEquals(0, $this->runAnalyzerOnFile('.negative-values.inc', ''));
 	}
 }
--- tests/units/Checks/TestParamsTypeCheck.php
+++ tests/units/Checks/TestParamsTypeCheck.php
@@ -17,49 +17,7 @@ class TestParamsTypeCheck extends TestSuiteSetup {
 	 * @rapid-unit Checks:ParamsTypeCheck:Function calling another function emits errors
 	 */
 	public function testFunctionInFunctionCallEmitsError() {
-		$this->assertEquals(4, $this->runAnalyzerOnFile('.1.inc', ErrorConstants::TYPE_FUNCTION_INSIDE_FUNCTION));
-	}
-	public function testParamTypeNoError() {
-		$this->assertEquals(0, $this->runAnalyzerOnFile('.2.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
+		$this->assertEquals(4, $this->runAnalyzerOnFile('.function-in-function.inc', ErrorConstants::TYPE_FUNCTION_INSIDE_FUNCTION));
 	}
 
-	public function testParamTypeErrors() {
-		$this->assertEquals(21, $this->runAnalyzerOnFile('.3.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	}
-
-	public function testParamTypeDefineNoError() {
-		$this->assertEquals(0, $this->runAnalyzerOnFile('.4.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	}
-
-	public function testParamTypeDefineErrors() {
-		$this->assertEquals(10, $this->runAnalyzerOnFile('.5.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	}
-
-	public function testParamTypeClassConstantsNoError() {
-		$this->assertEquals(0, $this->runAnalyzerOnFile('.6.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	}
-
-	public function testParamTypeClassConstantsErrors() {
-		$this->assertEquals(12, $this->runAnalyzerOnFile('.7.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	}
-
-	public function testParamTypeBitWiseNoError() {
-		$this->assertEquals(0, $this->runAnalyzerOnFile('.8.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	}
-
-	public function testParamTypeBitWiseErrors() {
-		$this->assertEquals(14, $this->runAnalyzerOnFile('.9.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	}
-
-	public function testParamTypeNegativeNoError() {
-		$this->assertEquals(0, $this->runAnalyzerOnFile('.10.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	}
-
-	public function testParamTypeCrossClassConstantsNoError() {
-		$this->assertEquals(0, $this->runAnalyzerOnFile('.11.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	}
-
-	public function testParamTypeCrossClassConstantsUntypedNoError() {
-		$this->assertEquals(0, $this->runAnalyzerOnFile('.12.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	}
 }
