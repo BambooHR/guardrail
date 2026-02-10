60ac5c1ada013ded3658e90b2deeddd3f9a347cd
fladolcetta <Fladolcetta@bamboohr.com>
2026-02-10 10:44:16 -0600

    Some cs fixes

--- tests/units/Checks/TestReturnCheck.php
+++ tests/units/Checks/TestReturnCheck.php
@@ -1,10 +1,11 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace BambooHR\Guardrail\Tests\Checks;
 
 use BambooHR\Guardrail\Checks\ErrorConstants;
 use BambooHR\Guardrail\Tests\TestSuiteSetup;
-
 
 /**
  * Class TestReturnCheck
@@ -12,11 +13,10 @@ use BambooHR\Guardrail\Tests\TestSuiteSetup;
  * @package BambooHR\Guardrail\Tests\Checks
  */
 class TestReturnCheck extends TestSuiteSetup {
-
 	public function testGenerators() {
 		// 6 valid generators (2x withYield, 2x withYieldFrom, interface, and abstract) = 0 errors
-		$this->assertEquals(0, $this->runAnalyzerOnFile('-generators.inc', ErrorConstants::TYPE_SIGNATURE_RETURN), "Failed to pass generator return types" );
+		$this->assertEquals(0, $this->runAnalyzerOnFile('-generators.inc', ErrorConstants::TYPE_SIGNATURE_RETURN), "Failed to pass generator return types");
 		// 6 invalid generators (3 class methods + 3 standalone functions) = 6 errors
-		$this->assertEquals(6, $this->runAnalyzerOnFile('-generators-fail.inc', ErrorConstants::TYPE_SIGNATURE_RETURN), "Failed to fail generator return types" );
+		$this->assertEquals(6, $this->runAnalyzerOnFile('-generators-fail.inc', ErrorConstants::TYPE_SIGNATURE_RETURN), "Failed to fail generator return types");
 	}
 }
