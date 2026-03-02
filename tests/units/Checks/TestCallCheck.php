9eb88fd882610c960dbbbca9bec4eb61bb2bd9b7
Joel <jidicula@bamboohr.com>
2026-03-02 01:26:08 -0600

    new tests and docs

--- tests/units/Checks/TestCallCheck.php
+++ tests/units/Checks/TestCallCheck.php
@@ -82,16 +82,12 @@ class TestCallCheck extends TestSuiteSetup {
 		$this->assertEquals(0, $this->runAnalyzerOnFile('.9.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
 	}
 
-	// public function testSplatParameterCount() {
-	// 	$this->assertEquals(0, $this->runAnalyzerOnFile('.10.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	// }
-
-	// public function testSplatParameterCountError() {
-	// 	$this->assertEquals(1, $this->runAnalyzerOnFile('.11.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
-	// }
-
+	/**
+	 * @return void
+	 * @rapid-unit Checks:CallCheck:Does not emit error when splat operator used on traversable array
+	 */
 	public function testSplatTraversable() {
-		$this->assertEquals(0, $this->runAnalyzerOnFile('.12.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
+		$this->assertEquals(0, $this->runAnalyzerOnFile('.10.inc', ErrorConstants::TYPE_SIGNATURE_TYPE));
 	}
 
 	/**
@@ -99,7 +95,7 @@ class TestCallCheck extends TestSuiteSetup {
 	 * @rapid-unit Checks:CallCheck:Does not emit error when passing value to template type parameter
 	 */
 	public function testTemplateTypeParameter() {
-		$this->assertEquals(0, $this->runAnalyzerOnFile('.13.inc', ErrorConstants::TYPE_SIGNATURE_TYPE, ['DocBlockGenerics' => true]));
+		$this->assertEquals(0, $this->runAnalyzerOnFile('.11.inc', ErrorConstants::TYPE_SIGNATURE_TYPE, ['DocBlockGenerics' => true]));
 	}
 
 }
